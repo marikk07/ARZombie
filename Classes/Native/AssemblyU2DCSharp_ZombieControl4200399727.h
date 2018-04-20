d07fe1211efab46c4b35f7e383dbc138d9ada8ad
pasichniak maryan <marikk07@ukr.net>
2018-04-20 12:40:07 +0300

    initial commit

---
+++ Classes/Native/AssemblyU2DCSharp_ZombieControl4200399727.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+
+#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <stdint.h>
+
+#include "UnityEngine_UnityEngine_MonoBehaviour1158329972.h"
+
+// UnityEngine.Animation
+struct Animation_t2068071072;
+
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// ZombieControl
+struct  ZombieControl_t4200399727  : public MonoBehaviour_t1158329972
+{
+public:
+	// System.Boolean ZombieControl::shouldMove
+	bool ___shouldMove_2;
+	// UnityEngine.Animation ZombieControl::animation
+	Animation_t2068071072 * ___animation_3;
+
+public:
+	inline static int32_t get_offset_of_shouldMove_2() { return static_cast<int32_t>(offsetof(ZombieControl_t4200399727, ___shouldMove_2)); }
+	inline bool get_shouldMove_2() const { return ___shouldMove_2; }
+	inline bool* get_address_of_shouldMove_2() { return &___shouldMove_2; }
+	inline void set_shouldMove_2(bool value)
+	{
+		___shouldMove_2 = value;
+	}
+
+	inline static int32_t get_offset_of_animation_3() { return static_cast<int32_t>(offsetof(ZombieControl_t4200399727, ___animation_3)); }
+	inline Animation_t2068071072 * get_animation_3() const { return ___animation_3; }
+	inline Animation_t2068071072 ** get_address_of_animation_3() { return &___animation_3; }
+	inline void set_animation_3(Animation_t2068071072 * value)
+	{
+		___animation_3 = value;
+		Il2CppCodeGenWriteBarrier(&___animation_3, value);
+	}
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
---
+++ Classes/Native/Il2CppCompilerCalculateTypeValues_18Table.cpp
@@ -0,0 +1,85 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <cstring>
+#include <string.h>
+#include <stdio.h>
+#include <cmath>
+#include <limits>
+#include <assert.h>
+
+#include "class-internals.h"
+#include "codegen/il2cpp-codegen.h"
+#include "AssemblyU2DCSharp_UnityEngine_UI_BoxSlider_BoxSlid1774115848.h"
+#include "AssemblyU2DCSharp_UnityEngine_UI_BoxSlider_Axis3966514019.h"
+#include "AssemblyU2DCSharp_HSVUtil3885028383.h"
+#include "AssemblyU2DCSharp_HsvColor1057062332.h"
+#include "AssemblyU2DCSharp_ParticlePainter1073897267.h"
+#include "AssemblyU2DCSharp_UnityEngine_XR_iOS_UnityARAmbient680084560.h"
+#include "AssemblyU2DCSharp_ZombieControl4200399727.h"
+
+
+
+
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1800 = { sizeof (BoxSliderEvent_t1774115848), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1801 = { sizeof (Axis_t3966514019)+ sizeof (Il2CppObject), sizeof(int32_t), 0, 0 };
+extern const int32_t g_FieldOffsetTable1801[3] = 
+{
+	Axis_t3966514019::get_offset_of_value___1() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	0,
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1802 = { sizeof (HSVUtil_t3885028383), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1803 = { sizeof (HsvColor_t1057062332)+ sizeof (Il2CppObject), sizeof(HsvColor_t1057062332 ), 0, 0 };
+extern const int32_t g_FieldOffsetTable1803[3] = 
+{
+	HsvColor_t1057062332::get_offset_of_H_0() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	HsvColor_t1057062332::get_offset_of_S_1() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	HsvColor_t1057062332::get_offset_of_V_2() + static_cast<int32_t>(sizeof(Il2CppObject)),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1804 = { sizeof (ParticlePainter_t1073897267), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1804[14] = 
+{
+	ParticlePainter_t1073897267::get_offset_of_painterParticlePrefab_2(),
+	ParticlePainter_t1073897267::get_offset_of_minDistanceThreshold_3(),
+	ParticlePainter_t1073897267::get_offset_of_maxDistanceThreshold_4(),
+	ParticlePainter_t1073897267::get_offset_of_frameUpdated_5(),
+	ParticlePainter_t1073897267::get_offset_of_particleSize_6(),
+	ParticlePainter_t1073897267::get_offset_of_penDistance_7(),
+	ParticlePainter_t1073897267::get_offset_of_colorPicker_8(),
+	ParticlePainter_t1073897267::get_offset_of_currentPS_9(),
+	ParticlePainter_t1073897267::get_offset_of_particles_10(),
+	ParticlePainter_t1073897267::get_offset_of_previousPosition_11(),
+	ParticlePainter_t1073897267::get_offset_of_currentPaintVertices_12(),
+	ParticlePainter_t1073897267::get_offset_of_currentColor_13(),
+	ParticlePainter_t1073897267::get_offset_of_paintSystems_14(),
+	ParticlePainter_t1073897267::get_offset_of_paintMode_15(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1805 = { sizeof (UnityARAmbient_t680084560), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1805[2] = 
+{
+	UnityARAmbient_t680084560::get_offset_of_l_2(),
+	UnityARAmbient_t680084560::get_offset_of_m_Session_3(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1806 = { sizeof (ZombieControl_t4200399727), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1806[2] = 
+{
+	ZombieControl_t4200399727::get_offset_of_shouldMove_2(),
+	ZombieControl_t4200399727::get_offset_of_animation_3(),
+};
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
